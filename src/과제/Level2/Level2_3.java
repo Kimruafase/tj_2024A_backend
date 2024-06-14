2b6b02464a58fcf4ac81a65e41b6a6af25786e7d
kimruafase <ppooppplus33@gmail.com>
2024-06-14 18:19:47 +0900

    과제

--- src/과제/Level2/Level2_3.java
+++ src/과제/Level2/Level2_3.java
@@ -1,4 +1,12 @@
 package 과제.Level2;
 
 public class Level2_3 {
+    public static void main(String[] args) {
+        int korean = 89;
+        int math = 89;
+        int eng = 89;
+        int total = korean + math + eng;
+        double mean = (double) total / 3;
+
+    }
 }
