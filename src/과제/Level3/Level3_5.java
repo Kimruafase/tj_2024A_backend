013e559a8085e6d4a6a9162c7ca5675a381e378d
kimruafase <ppooppplus33@gmail.com>
2024-06-17 09:12:02 +0900

    과제

--- src/과제/Level3/Level3_5.java
+++ src/과제/Level3/Level3_5.java
@@ -4,13 +4,11 @@ import java.util.Random;
 
 public class Level3_5 {
     public static void main(String[] args) {
-        String s = " ";
         int random = new Random().nextInt(10);
-        for (int i = 0; i < 6; i++) {
-            s += random;
-            for (int j = 0; j < 6; j++) {
-                random = new Random().nextInt(10);
-            }
+        String s = String.valueOf(random);
+        for (int i = 0; i < 5; i++) {
+            int random1 = new Random().nextInt(10);
+            s += random1;
         }
         System.out.println("본인확인 인증번호 : " + s);
     }
