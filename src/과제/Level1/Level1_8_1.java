f0d338ae9ec7effaa575864829cc166df7631c35
kimruafase <ppooppplus33@gmail.com>
2024-06-18 09:31:19 +0900

    day08

--- src/과제/Level1/Level1_8_1.java
+++ src/과제/Level1/Level1_8_1.java
@@ -18,24 +18,72 @@ public class Level1_8_1 {
         boardlist = scanner.nextLine();
 
         System.out.print("[1] 방문록 : ");
-        output = scanner.nextLine();
+        boardlist += " \t";
+        boardlist += scanner.nextLine();
+        output += boardlist;
 
         System.out.println(header);
-        System.out.printf("1        %s \t " ,boardlist);
-        System.out.println(output);
+        System.out.printf("1        %s \t \n" ,boardlist);
         System.out.println(footer);
 
         System.out.print("[2] 작성자 : ");
+        boardlist = "";
         boardlist += scanner.nextLine();
 
         System.out.print("[2] 방문록 : ");
-        output += scanner.nextLine();
+        boardlist += "\t";
+        boardlist += scanner.nextLine();
+
 
         System.out.print(header);
-        System.out.printf("1        %s \t " ,boardlist);
-        System.out.println(output);
+        System.out.printf("1        %s \t \n" ,output);
+        System.out.printf("2        %s \t \n" ,boardlist);
         System.out.println(footer);
 
+        output += "\n2        ";
+        output += boardlist;
+        System.out.print("[3] 작성자 : ");
+        boardlist = "";
+        boardlist += scanner.nextLine();
+
+        System.out.print("[3] 방문록 : ");
+        boardlist += "\t";
+        boardlist += scanner.nextLine();
+
+        System.out.print(header);
+        System.out.printf("1        %s \t \n" ,output);
+        System.out.printf("3        %s \t \n" ,boardlist);
+        System.out.println(footer);
+
+        output += "\n3        ";
+        output += boardlist;
+        System.out.print("[4] 작성자 : ");
+        boardlist = "";
+        boardlist += scanner.nextLine();
+
+        System.out.print("[4] 방문록 : ");
+        boardlist += "\t";
+        boardlist += scanner.nextLine();
+
+        System.out.print(header);
+        System.out.printf("1        %s \t \n" ,output);
+        System.out.printf("4        %s \t \n" ,boardlist);
+        System.out.println(footer);
+
+        output += "\n4        ";
+        output += boardlist;
+        System.out.print("[5] 작성자 : ");
+        boardlist = "";
+        boardlist += scanner.nextLine();
+
+        System.out.print("[5] 방문록 : ");
+        boardlist += "\t";
+        boardlist += scanner.nextLine();
+
+        System.out.print(header);
+        System.out.printf("1        %s \t \n" ,output);
+        System.out.printf("5        %s \t \n" ,boardlist);
+        System.out.println(footer);
         /* ----------- */
     }
 }
